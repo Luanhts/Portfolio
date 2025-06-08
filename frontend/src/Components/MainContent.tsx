25d37ee3cd7a2449433b8924589bf2d6eca72ba7
Luanhts <luanh3nrique08@gmail.com>
2025-06-08 14:27:43 -0300

    feat: adicionadno imgs e ajustes dos meus projetos

--- frontend/src/Components/MainContent.tsx
+++ frontend/src/Components/MainContent.tsx
@@ -4,8 +4,7 @@ export function MainContent() {
     <section className="flex flex-col-reverse md:flex-row items-center md:items-start justify-center gap-12 m-6 md:m-24">
       <div className="flex-1 text-center md:text-left text-[#21243D] max-w-xl">
         <h1 className="font-bold text-4xl md:text-5xl leading-tight mb-4">
-          Hi, I am Luan, <br />
-          Developer
+          Olá, me chamo Luan
         </h1>
         <p className="text-gray-600 text-base md:text-lg leading-relaxed max-w-lg mx-auto">
   Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora totam
--- frontend/src/Components/SectionThree.tsx
+++ frontend/src/Components/SectionThree.tsx
@@ -1,27 +1,27 @@
-import perfil from "../assets/imgs/perfilft.png"
+import projectImg2 from "../assets/imgs/project-img2.png"
 
 export function SectionThree() {
   return (
     <section className="px-4 md:px-20 py-16 bg-white">
       <h1 className="text-3xl font-semibold text-[#21243D] mb-12 text-center md:text-center">
-        Featured works
+        Meus Projetos
       </h1>
 
       <div className="space-y-12 max-w-6xl mx-auto">
         {/* Card 1 */}
-        <div className="flex flex-col md:flex-row gap-6 border-b pb-10">
+        <div className="flex flex-col md:flex-row gap-6 border-b border-[#E0E0E0] pb-10">
           <img
-            src={perfil}
+            src={projectImg2}
             alt="Designing Dashboards"
             className="w-full md:w-64 h-auto rounded-md object-cover"
           />
           <div className="flex flex-col justify-center">
-            <h2 className="text-2xl font-bold text-[#21243D]">Designing Dashboards</h2>
+            <h2 className="text-2xl font-bold text-[#21243D]">Sistema Web ( MondeLuxe )</h2>
             <div className="flex items-center gap-4 my-3">
               <span className="bg-[#FF4C61] text-white text-sm px-3 py-1 rounded font-bold">
-                2020
+                2025
               </span>
-              <span className="text-[#8695A4] text-sm">Dashboard</span>
+              <span className="text-[#8695A4] text-sm">Gestão</span>
             </div>
             <p className="text-[#21243D] text-base leading-relaxed">
               Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
@@ -32,19 +32,19 @@ export function SectionThree() {
         </div>
 
         {/* Card 2 */}
-        <div className="flex flex-col md:flex-row gap-6 border-b pb-10">
+        <div className="flex flex-col md:flex-row gap-6 border-b border-[#E0E0E0] pb-10">
           <img
-            src={perfil}
+            src={projectImg2}
             alt="Vibrant Portraits of 2020"
             className="w-full md:w-64 h-auto rounded-md object-cover"
           />
           <div className="flex flex-col justify-center">
-            <h2 className="text-2xl font-bold text-[#21243D]">Vibrant Portraits of 2020</h2>
+            <h2 className="text-2xl font-bold text-[#21243D]">Controle Fácil</h2>
             <div className="flex items-center gap-4 my-3">
               <span className="bg-[#FF4C61] text-white text-sm px-3 py-1 rounded font-bold">
-                2018
+                2025
               </span>
-              <span className="text-[#8695A4] text-sm">Illustration</span>
+              <span className="text-[#8695A4] text-sm">Sistema Web</span>
             </div>
             <p className="text-[#21243D] text-base leading-relaxed">
               Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
@@ -55,19 +55,43 @@ export function SectionThree() {
         </div>
 
         {/* Card 3 */}
-        <div className="flex flex-col md:flex-row gap-6">
+        <div className="flex flex-col md:flex-row gap-6 border-b border-[#E0E0E0] pb-10 ">
           <img
-            src={perfil}
+            src={projectImg2}
             alt="36 Days of Malayalam type"
             className="w-full md:w-64 h-auto rounded-md object-cover"
           />
           <div className="flex flex-col justify-center">
-            <h2 className="text-2xl font-bold text-[#21243D]">36 Days of Malayalam type</h2>
+            <h2 className="text-2xl font-bold text-[#21243D]">TSW PG ( Save Your Future )</h2>
             <div className="flex items-center gap-4 my-3">
               <span className="bg-[#FF4C61] text-white text-sm px-3 py-1 rounded font-bold">
-                2018
+                2025
               </span>
-              <span className="text-[#8695A4] text-sm">Typography</span>
+              <span className="text-[#8695A4] text-sm">Hackathon</span>
+            </div>
+            <p className="text-[#21243D] text-base leading-relaxed">
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
+              officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud
+              amet.
+            </p>
+          </div>
+        </div>
+
+
+        {/* Card 4 */}
+        <div className="flex flex-col md:flex-row gap-6">
+          <img
+            src={projectImg2}
+            alt="36 Days of Malayalam type"
+            className="w-full md:w-64 h-auto rounded-md object-cover"
+          />
+          <div className="flex flex-col justify-center">
+            <h2 className="text-2xl font-bold text-[#21243D]">Smart Report</h2>
+            <div className="flex items-center gap-4 my-3">
+              <span className="bg-[#FF4C61] text-white text-sm px-3 py-1 rounded font-bold">
+                2024
+              </span>
+              <span className="text-[#8695A4] text-sm">Web App</span>
             </div>
             <p className="text-[#21243D] text-base leading-relaxed">
               Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
